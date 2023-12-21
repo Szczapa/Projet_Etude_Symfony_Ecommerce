2972ca33cbac2ae71b37c258c7385957a632b5b3
Szczapa <98310905+Szczapa@users.noreply.github.com>
2023-12-21 13:19:34 +0100

    Update Mail.php

--- src/Classes/Mail.php
+++ src/Classes/Mail.php
@@ -9,7 +9,7 @@ class Mail
 {
     public function send($to_email, $to_name, $subject, $content)
     {
-        $mj = new Client('31ef18dd0f93b2d93654d535e9027e21', '6555dd2a5ffea8587594b12db2f6f83f', true, ['version' => 'v3.1']);
+        $mj = new Client('', '', true, ['version' => 'v3.1']);
 
         $body = [
         'Messages' => [
